21965d97048852856f1c05b78c09b5c35a390e6d
21Bhang <uhuribhang211@gmail.com>
2026-08-20 15:22:08 +0300

    adding the Chapter 3: Chapter 3: React and GraphQL

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
   cache: new InMemoryCache(),
 })
 
